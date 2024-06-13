c76afdaab4e75eb5752f66345a68705f6ac2ca6a
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2024-06-13 13:07:48 +0900

    フレームレート変更

--- Assets/Script/TitleScript.cs
+++ Assets/Script/TitleScript.cs
@@ -42,7 +42,7 @@ public class TitleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
